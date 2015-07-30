d4f252abab2ed8bbd29489fe376eb596894c8cb8
maditnerd <sarrailh.remi@gmail.com>
2015-07-30 14:04:21 +0200

    Update to new version

--- python/growl.py
+++ python/growl.py
@@ -1,36 +1,36 @@
 #!/usr/bin/python
 # use standard Python logging
 import logging
-logging.basicConfig(level=logging.INFO)
 import gntp.notifier
 import sys
+logging.basicConfig(level=logging.INFO)
 
 if len(sys.argv) > 3:
-	hostname = sys.argv[1]
-	password = sys.argv[2] 
-	title = sys.argv[3]
-	description = sys.argv[4]
+    hostname = sys.argv[1]
+    password = sys.argv[2]
+    title = sys.argv[3]
+    description = sys.argv[4]
 
-	growl = gntp.notifier.GrowlNotifier(
-		applicationName = sys.argv[2],
-		notifications = ["New Updates","New Messages"],
-		defaultNotifications = ["New Messages"],
-	hostname = hostname, # Here enter your IP address
-	password = password # Here enter your growl password
-	)
-	growl.register()
-	# Send one message
-	#image = open('/var/www/tmp/image.jpg', 'rb').read()
-	growl.notify(
-		noteType = "New Messages",
-		title = title,
-		description = description,
- 		#icon = image, #you can optionally define an image icon to appear with the notification
- 		sticky = True,
- 		priority = 1,
-	)
+    growl = gntp.notifier.GrowlNotifier(
+        applicationName=sys.argv[2],
+        notifications=["New Updates", "New Messages"],
+        defaultNotifications=["New Messages"],
+        hostname=hostname,  # Here enter your IP address
+        password=password  # Here enter your growl password
+    )
+    growl.register()
+    # Send one message
+    # image = open('/var/www/tmp/image.jpg', 'rb').read()
+    growl.notify(
+        noteType="New Messages",
+        title=title,
+        description=description,
+        # icon = image, #you can optionally define an image icon to appear with the notification
+        sticky=True,
+        priority=1,
+    )
 else:
-	print "You didn't provide enough argument"
-	print "-----------------------------------------------"
-	print "Usage: growl.py IP PASSWORD Title Description"
-	print "-----------------------------------------------"
+    print "You didn't provide enough argument"
+    print "-----------------------------------------------"
+    print "Usage: growl.py IP PASSWORD Title Description"
+    print "-----------------------------------------------"
